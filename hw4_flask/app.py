2974eb0797960dcea506328d0c245d49c125797c
Pilot93 <pilotfdd@gmail.com>
2020-06-01 13:06:46 +0300

    addong POST

--- hw4_flask/app.py
+++ hw4_flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import pandas as ps
 
 data = ps.read_table('tes_data.tsv')
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '/movie_id – search by tag\n /movie – search by title\n /year – search by year'
+    return '/movie_id – search by tag\n /movie – search by title\n /year – search by year\n'
 
 @app.route('/movie_id/<idfilm>')
 def movie(idfilm):
@@ -32,6 +32,12 @@ def mov_by_year(year_foo):
     else:
         return '', 404
 
+@app.route('/suggest/<int:topk>', methods=['POST'])
+def recommend(topk):
+    data = request.get_json()
+    print(data['likes'])
+
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(port=5000)
